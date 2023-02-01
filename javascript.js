475ddfd3b93d208fe79ca9185db37a0a7c11c0e3
Christian <christiancharlton17@gmail.com>
2023-02-01 10:18:21 -0700

    Create user interface

---
+++ javascript.js
@@ -0,0 +1,42 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function operate(operator, n1, n2) {
+    let result = 0;
+    
+    switch (operator) {
+        case '+':
+            result = add(n1, n2);
+            break;
+
+        case '-':
+            result = subtract(n1, n2);
+            break;
+
+        case '/':
+            result = divide(n1, n2);
+            break;
+
+        case '*':
+            result = multiply(n1, n2);
+            break;
+
+        default:
+            return 'ERROR!'
+    }
+
+    return result;
+}
