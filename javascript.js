0509243a2b7ee80325c2bc82ce193ab1f295d76a
Christian <christiancharlton17@gmail.com>
2023-02-02 07:58:24 -0700

    Store operator

--- javascript.js
+++ javascript.js
@@ -1,10 +1,43 @@
 let controlsContainer = document.querySelector('.container');
 let display = document.querySelector('.display');
+let displayValue = '';
+let evaluateNext = false;
+
+let operator = '';
 
 controlsContainer.addEventListener('click', (e) => {
-    let userValue = e.target.value;
-    if (userValue) {
-        display.textContent+=userValue;
+    let userInput = e.target.value;
+    if (userInput) {
+        // check if current display string index is an operator
+        // if it is don't add anything
+        // helps prevent user from chaining multiple operators and improves UX
+
+        switch (userInput) {
+            case '+':
+                operator = '+';
+                break;
+            case '-':
+                operator = '-'
+                break;
+            case 'x':
+                operator = 'x'
+                break;
+            case '/':
+                operator = '/'
+                break;
+        
+            default:
+                break;
+        }
+
+        
+        displayValue += userInput !== '=' ? userInput : '' ;
+
+        if (userInput === '=') {
+            operate(displayValue);
+        }
+        
+        display.textContent += userInput;
     }
 });
 
@@ -48,5 +81,5 @@ function operate(operator, n1, n2) {
             return 'ERROR!'
     }
 
-    return result;
+    display += result;
 }
